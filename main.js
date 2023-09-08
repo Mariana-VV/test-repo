a5d981aba2f8141fa5026e47883b3036afc14d08
Mariana <63500992+Mariana-VV@users.noreply.github.com>
2023-09-08 17:22:03 +0200

    repo

--- main.js
+++ main.js
@@ -6,26 +6,27 @@ const colors = [
 ];
 
 function  createColorCardsMarkup(colors)  {
-  return  colors.map((color)=>{
-    return `
-    <div class="color-card">
+  const markup = colors.map((color)=>{
+    return `<div class="color-card" style = 'display: flex;'>
         <div  
         class="color-swatch"  
         data-hex="${color.hex}"  
         data-rgb="${color.rgb}"  
-        style="background-color: ${color.hex}">
+        style= "background-color:${color.hex}; margin-right: 20px;margin-bottom:50px"
         
-        </div>
-    <div class="color-meta">
+        ></div>
+    
+    <div class="color-meta" style="margin-top:100px">
         <p>${color.hex}</p>
         <p>${color.rgb}</p>
     </div>
 </div>
 `})
-.join(" ");
+.join("");
 
+console.log(markup);
+return markup;
 }
-
 const palleteContainer = document.querySelector('.js-palette');
 const markUp = createColorCardsMarkup(colors);
 palleteContainer.insertAdjacentHTML('afterbegin', markUp);
@@ -36,5 +37,9 @@ function onPalleteContainerClick(event) {
   if(!event.target.classList.contains('color-swatch') ) {
     return;
   }
-  console.log(event.target);
+  const swatchEl = event.target;
+  const parentColorCard = swatchEl.closest('.color-card');
+  parentColorCard.classList.add('active');
+  document.body.style.backgroundColor = swatchEl.dataset.hex;
 }
+
